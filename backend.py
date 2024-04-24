d55c03482319160ffcfe36fb22f68ce303873467
omaremad02 <89496360+omaremad02@users.noreply.github.com>
2024-04-24 17:02:32 +0200

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 
+
 class Board:
     def __init__(self):
         # Initialize an empty Sudoku grid
@@ -17,9 +18,9 @@ class Board:
     def is_valid_move(self, row, col, value):
         # Check if placing a value in a cell is a valid move
         return (
-            self.is_valid_row(row, value) and
-            self.is_valid_column(col, value) and
-            self.is_valid_subgrid(row, col, value)
+                self.is_valid_row(row, value) and
+                self.is_valid_column(col, value) and
+                self.is_valid_subgrid(row, col, value)
         )
 
     def is_valid_row(self, row, value):
@@ -37,25 +38,24 @@ class Board:
         subgrid_values = self.grid[subgrid_row_start:subgrid_row_start + 3, subgrid_col_start:subgrid_col_start + 3]
         return value not in subgrid_values
 
+
 def generate_sudoku_puzzle():
     grid = np.zeros((9, 9), dtype=int)
 
-    # Populate the diagonal subgrids (3x3)
-    for i in range(0, 9, 3):
-        subgrid = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        random.shuffle(subgrid)
+    # Populate the subgrids randomly
+    for i in range(3):
         for j in range(3):
-            for k in range(3):
-                grid[i + j][i + k] = subgrid.pop()
+            subgrid = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            random.shuffle(subgrid)
+            for m in range(3):
+                for n in range(3):
+                    grid[i * 3 + m][j * 3 + n] = subgrid.pop()
 
-    # Solve the Sudoku puzzle
-    #solve_sudoku(grid)
-
-    # Remove some numbers to create the puzzle
-    remove_count = random.randint(30, 50)
-    for _ in range(remove_count):
-        row = random.randint(0, 8)
-        col = random.randint(0, 8)
+    # Remove all numbers except for 10 to create the puzzle
+    remaining_cells = [(i, j) for i in range(9) for j in range(9)]
+    random.shuffle(remaining_cells)
+    for _ in range(81 - 15):
+        row, col = remaining_cells.pop()
         grid[row][col] = 0
 
     return grid
