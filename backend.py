a15cf1b6acf0b5cc12b0568aef7c0f04e6ab877d
Borg2 <mohamedborg2002@gmail.com>
2024-04-25 19:07:05 +0200

    fixed the generate sudoku func

--- backend.py
+++ backend.py
@@ -13,7 +13,7 @@ class Board:
 
     def get_value(self, row, col) -> int:
         # Get the value of a cell in the Sudoku grid
-        return self.grid[row, col]
+        return self.grid[row][col]
 
     def is_valid_move(self, row, col, value):
         # Check if placing a value in a cell is a valid move
@@ -40,21 +40,24 @@ class Board:
 
     def generate_sudoko_puzzle(self):
         grid = np.zeros((9, 9), dtype=int)
-        for i in range(3):
-            for j in range(3):
-                subgrid = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-                for m in range(3):
-                    for n in range(3):
-                        random.shuffle(subgrid)
-                        grid[i * 3 + m][j * 3 + n] = subgrid.pop()
+
+        numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        for i in range(9):
+            for j in range(9):
+                random.shuffle(numbers)
+                for num in numbers:
+                    if self.is_valid_move(i, j, num):
+                        self.grid[i][j] = num
+                        break
 
         # Remove all numbers except for 10 to create the puzzle
         remaining_cells = [(i, j) for i in range(9) for j in range(9)]
         random.shuffle(remaining_cells)
         for _ in range(81 - 15):
             row, col = remaining_cells.pop()
-            grid[row][col] = 0
-        self.grid = grid
+            self.grid[row][col] = 0
+        
+        
         return self.grid
 
     def generate_empty_sudoko(self):
@@ -63,27 +66,5 @@ class Board:
 
 
 
-def is_valid_sudoku(grid):
-    # Check rows and columns
-    print("1")
-    for i in range(9):
-        row = set()
-        col = set()
-        for j in range(9):
-            if grid[i][j] and (grid[i][j] in row or grid[j][i] in col):
-                return False
-            row.add(grid[i][j])
-            col.add(grid[j][i])
 
-    # Check subgrids
-    for i in range(0, 9, 3):
-        for j in range(0, 9, 3):
-            subgrid = set()
-            for m in range(3):
-                for n in range(3):
-                    if grid[i+m][j+n] in subgrid and grid[i+m][j+n]:
-                        return False
-                    subgrid.add(grid[i+m][j+n])
-
-    return True
 
